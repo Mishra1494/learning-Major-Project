4b803ed3d926b37d8020e3058c1ffd4447dd20f2
ROSHANMISHRA <roshanmishra200407@gmail.com>
2024-12-18 02:40:54 +0530

    last commit of 17 dec  night

--- ClassRoom/server.js
+++ ClassRoom/server.js
@@ -2,20 +2,57 @@ const express = require("express");
 const app = express();
 const user = require("./routes/user.js");
 const post = require("./routes/post.js");
-
-app.use("/users",user);
-app.use("/post",post);
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 app.listen(3000,()=>{
     console.log("hello from express routing ");
 })
 
 
-// this is root route
 
-app.get("/",(req,res)=>{
-    res.send("wah express rout is working");
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({secret: 'keyboard cat'}))
+
+
+app.get("/test",(req,res)=>{
+    res.send("test succeed!");
 })
 
+// app.use(cookieParser("secretcode"));
+// app.use("/users",user);
+// app.use("/post",post);
 
-app.use("/users",user);
-app.use("/post",post);
+// // parsing the cookie
+// app.get("/",(req,res)=>{
+//     console.dir(req.cookies);
+//     res.send("wah express rout is working");
+// })
+
+// // sending cookies
+// app.get("/getcookies",(req,res)=>{
+//     res.cookie("great","nameste");
+//     res.cookie("origin","India");
+//     res.send("WE set you a cookie");
+// })
+
+// // defining the signed cookie
+// app.get("/getSignedcookie",(req,res)=>{
+//     res.cookie("color","red",{signed : true});
+//     res.send("signec cookie is defined done!");
+// })
+
+
+// // verifying signed cookie 
+// app.get("/verify",(req,res)=>{
+//     console.log(req.signedCookies);
+//     res.send(req.signedCookies);
+//     // res.send("got signed cookie");
+// })
+// // getting the cookie
+// app.get("/greet",(req,res)=>{
+//     let {name = "anonymous"} = req.cookies;
+//     res.send(`hi, ${name}`);
+// })
+
+
+
